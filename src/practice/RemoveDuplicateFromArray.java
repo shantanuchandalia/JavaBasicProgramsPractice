141272e67cd2be12eaf42778873fa816b127aa02
Shantanu Chandalia <shantanu.chandalia@gmail.com>
2020-04-27 19:52:40 +0530

    Remove Duplicate element from Array List | unFinished
    Uselessly wasting time.

---
+++ src/practice/RemoveDuplicateFromArray.java
@@ -0,0 +1,21 @@
+package practice;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class RemoveDuplicateFromArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int intArr[] = new int[n];
+		int j=0;
+		int intArr[] = {12,3,33,56,22,12,34,2,11,5,5,3,12,56};
+		ArrayList <Integer> al = new ArrayList <Integer> ();
+		
+		int tempArr[] = new int[intArr.length];
+		
+		
+	}
+
+}
