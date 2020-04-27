90daf7d349962d4b71c968eb4ce8499b4d7f7e04
Shantanu Chandalia <shantanu.chandalia@gmail.com>
2020-04-27 18:12:52 +0530

    esolved basic exception

--- src/practice/encryption_SHA_256.java
+++ src/practice/encryption_SHA_256.java
@@ -1,10 +1,12 @@
-import java.io.*;
+package practice;
+
+
 import java.util.*;
 import java.util.regex.Pattern;
 import java.security.*;
-import java.nio.charset.Charset;
 
-public class Solution {
+
+public class encryption_SHA_256{
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
