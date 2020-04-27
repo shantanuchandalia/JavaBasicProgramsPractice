f952655bb834e134124acb6738399c795c2608b0
Shantanu Chandalia <shantanu.chandalia@gmail.com>
2020-04-27 18:55:02 +0530

    added Integer Palindrome program!

--- src/practice/IntegerPalindrome.java
+++ src/practice/IntegerPalindrome.java
@@ -1,10 +1,25 @@
 package practice;
 
+import java.util.Scanner;
+
 public class IntegerPalindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num = 11011;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter an integer to check for Integer Palindrome:");
+		int input = sc.nextInt();
+		int num = input;
+		int rev=0;
+		while(num!=0)
+		{
+			rev = rev *10 + (num % 10);
+			num=num/10;
+		}
+		if (input == rev)
+			System.out.println("Its an Integer Palidrome");
+		else
+			System.out.println("Its not an Integer Palindrome");
 		
 	}
 
